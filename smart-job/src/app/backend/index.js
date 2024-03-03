5a17f2245e4a6f4163e50ad01e721630651b0ac7
fpsanger <47747956+fpsanger@users.noreply.github.com>
2024-03-03 16:37:23 -0300

    criacao endpoint get vagas (#13)
    Co-authored-by: Felipe Sanger

--- smart-job/src/app/backend/index.js
+++ smart-job/src/app/backend/index.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 const app = express();
 
 const usuarioRoute = require("./rotas/usuario");
+const vagasRoute = require("./rotas/vagas");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/usuario", usuarioRoute);
+app.use("/vagas", vagasRoute);
 
 app.listen(3000, () => {
   console.log("Servidor Express rodando na porta 3000");
---
+++ smart-job/src/app/backend/rotas/vagas.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const sql = require("../db");
+
+const routes = express.Router();
+
+routes.get("/", async (req, res) => {
+  try {
+    const results = await sql.query("SELECT * FROM Vaga");
+    res.status(200).json(results);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+module.exports = routes;
