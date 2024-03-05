cecb7f82fbfb24e784a15988289bef9092237328
Felipe Sanger <FSanger@czarnikow.com>
2024-03-05 10:46:24 -0300

    fix vaga test

--- smart-job/src/app/vaga/vaga.component.spec.ts
+++ smart-job/src/app/vaga/vaga.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VagaComponent } from './vaga.component';
-import { ActivatedRoute } from '@angular/router';
-import { Router } from 'express';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { VagaService } from '../services/vaga.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('VagaComponent', () => {
   let component: VagaComponent;
@@ -14,9 +14,20 @@ describe('VagaComponent', () => {
     await TestBed.configureTestingModule({
       providers: [
         { provide: VagaService },
+        { provide: HttpClient },
+        { provide: HttpHandler },
         { provide: MessageService },
-        { provide: Router },
-        { provide: ActivatedRoute },
+        { provide: Router, useValue: {} },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: {
+                id: '1',
+              },
+            },
+          },
+        },
       ],
       declarations: [VagaComponent],
     }).compileComponents();
