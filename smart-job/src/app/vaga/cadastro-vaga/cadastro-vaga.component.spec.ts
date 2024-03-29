f5df9cb656a9b5b8158ab8561c811ce696c38319
Felipe Sanger <FSanger@czarnikow.com>
2024-03-29 18:37:16 -0300

    testes vagas

--- smart-job/src/app/vaga/cadastro-vaga/cadastro-vaga.component.spec.ts
+++ smart-job/src/app/vaga/cadastro-vaga/cadastro-vaga.component.spec.ts
@@ -7,18 +7,27 @@ import { DatePipe } from '@angular/common';
 import { MessageService } from 'primeng/api';
 import { VagaService } from 'src/app/services/vaga.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { of } from 'rxjs';
+import { IVaga } from 'src/app/interfaces/vaga.interface';
 
 describe('CadastroVagaComponent', () => {
   let component: CadastroVagaComponent;
   let fixture: ComponentFixture<CadastroVagaComponent>;
 
-  const mockAuthService = {
-    getTokenData() {
-      return { id: 1 };
-    },
-  };
+  let authService: jasmine.SpyObj<AuthService>;
+  let vagaService: jasmine.SpyObj<VagaService>;
+  let messageService: jasmine.SpyObj<MessageService>;
 
   beforeEach(async () => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', [
+      'getTokenData',
+    ]);
+    const vagaServiceSpy = jasmine.createSpyObj('VagaService', [
+      'adicionarVaga',
+      'editarVaga',
+    ]);
+    const messageServiceSpy = jasmine.createSpyObj('MessageService', ['add']);
+
     await TestBed.configureTestingModule({
       providers: [
         { provide: HttpClient },
@@ -26,18 +35,190 @@ describe('CadastroVagaComponent', () => {
         { provide: VagaService },
         { provide: FormBuilder },
         { provide: DatePipe },
-        { provide: MessageService },
-        { provide: AuthService, useValue: mockAuthService },
+        { provide: VagaService, useValue: vagaServiceSpy },
+        { provide: MessageService, useValue: messageServiceSpy },
+        { provide: AuthService, useValue: authServiceSpy },
       ],
       declarations: [CadastroVagaComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CadastroVagaComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    vagaService = TestBed.inject(VagaService) as jasmine.SpyObj<VagaService>;
+    messageService = TestBed.inject(
+      MessageService
+    ) as jasmine.SpyObj<MessageService>;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create form', () => {
+    const tokenData = {
+      id: 1,
+      nome: 'Nome',
+      Email: 'teste@teste.com',
+      Telefone: '123456789',
+      CPF: '12345678900',
+      CNPJ: '12345678000100',
+      Senha: '123',
+      Ativo: true,
+    };
+
+    authService.getTokenData.and.returnValue(tokenData);
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(component.form.contains('nomeVaga')).toBeTruthy();
+    expect(component.form.contains('descricao')).toBeTruthy();
+    expect(component.form.contains('remuneracao')).toBeTruthy();
+    expect(component.form.contains('endereco')).toBeTruthy();
+    expect(component.form.contains('estado')).toBeTruthy();
+    expect(component.form.contains('cidade')).toBeTruthy();
+    expect(component.form.contains('tipoVaga')).toBeTruthy();
+    expect(component.form.contains('dataExpiracao')).toBeTruthy();
+    expect(component.form.contains('limiteTrabalhadores')).toBeTruthy();
+  });
+
+  it('should validate form fields', () => {
+    const tokenData = {
+      id: 1,
+      nome: 'Nome',
+      Email: 'teste@teste.com',
+      Telefone: '123456789',
+      CPF: '12345678900',
+      CNPJ: '12345678000100',
+      Senha: '123',
+      Ativo: true,
+    };
+
+    authService.getTokenData.and.returnValue(tokenData);
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    let nomeVaga = component.form.get('nomeVaga');
+    nomeVaga.setValue('');
+    expect(nomeVaga.valid).toBeFalsy();
+
+    let descricao = component.form.get('descricao');
+    descricao.setValue('');
+    expect(descricao.valid).toBeFalsy();
+
+    let remuneracao = component.form.get('remuneracao');
+    remuneracao.setValue('');
+    expect(remuneracao.valid).toBeFalsy();
+
+    let endereco = component.form.get('endereco');
+    endereco.setValue('');
+    expect(endereco.valid).toBeFalsy();
+
+    let estado = component.form.get('estado');
+    estado.setValue('');
+    expect(estado.valid).toBeFalsy();
+
+    let cidade = component.form.get('cidade');
+    cidade.setValue('');
+    expect(cidade.valid).toBeFalsy();
+
+    let tipoVaga = component.form.get('tipoVaga');
+    tipoVaga.setValue('');
+    expect(tipoVaga.valid).toBeFalsy();
+
+    let dataExpiracao = component.form.get('dataExpiracao');
+    dataExpiracao.setValue('');
+    expect(dataExpiracao.valid).toBeFalsy();
+
+    let limiteTrabalhadores = component.form.get('limiteTrabalhadores');
+    nomeVaga.setValue('');
+    expect(limiteTrabalhadores.valid).toBeFalsy();
+  });
+
+  it('should adicionarVaga', () => {
+    const tokenData = {
+      id: 1,
+      nome: 'Nome',
+      Email: 'teste@teste.com',
+      Telefone: '123456789',
+      CPF: '12345678900',
+      CNPJ: '12345678000100',
+      Senha: '123',
+      Ativo: true,
+    };
+
+    authService.getTokenData.and.returnValue(tokenData);
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    component.form.setValue({
+      nomeVaga: 'Desenvolvedor',
+      descricao: 'Descrição da vaga',
+      cidade: 'Cidade',
+      endereco: 'Endereço',
+      estado: 'Estado',
+      remuneracao: 5000,
+      tipoVaga: 'CLT',
+      dataExpiracao: new Date(),
+      limiteTrabalhadores: 1,
+    });
+
+    component.vaga = { Id: null } as IVaga;
+    vagaService.adicionarVaga.and.returnValue(of(null));
+
+    component.salvarVaga();
+
+    expect(vagaService.adicionarVaga).toHaveBeenCalled();
+    expect(messageService.add).toHaveBeenCalledWith({
+      severity: 'success',
+      summary: 'Sucesso',
+      detail: 'Vaga adicionada com sucesso',
+    });
+  });
+
+  it('should editarVaga', () => {
+    const tokenData = {
+      id: 1,
+      nome: 'Nome',
+      Email: 'teste@teste.com',
+      Telefone: '123456789',
+      CPF: '12345678900',
+      CNPJ: '12345678000100',
+      Senha: '123',
+      Ativo: true,
+    };
+
+    authService.getTokenData.and.returnValue(tokenData);
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    component.form.setValue({
+      nomeVaga: 'Desenvolvedor',
+      descricao: 'Descrição da vaga',
+      cidade: 'Cidade',
+      endereco: 'Endereço',
+      estado: 'Estado',
+      remuneracao: 5000,
+      tipoVaga: 'CLT',
+      dataExpiracao: new Date(),
+      limiteTrabalhadores: 1,
+    });
+
+    component.vaga = { Id: 1 } as IVaga;
+    vagaService.editarVaga.and.returnValue(of(null));
+
+    component.salvarVaga();
+
+    expect(vagaService.editarVaga).toHaveBeenCalled();
+    expect(messageService.add).toHaveBeenCalledWith({
+      severity: 'success',
+      summary: 'Sucesso',
+      detail: 'Vaga editada com sucesso',
+    });
   });
 });
--- smart-job/src/app/vaga/detalhe-vaga/detalhe-vaga.component.spec.ts
+++ smart-job/src/app/vaga/detalhe-vaga/detalhe-vaga.component.spec.ts
@@ -6,47 +6,218 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { VagaService } from 'src/app/services/vaga.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { of } from 'rxjs';
+import { ITrabalhadorVaga } from 'src/app/interfaces/trabalhador-vaga';
+import { IVaga } from 'src/app/interfaces/vaga.interface';
 
 describe('DetalheVagaComponent', () => {
   let component: VagaComponent;
   let fixture: ComponentFixture<VagaComponent>;
 
-  const mockAuthService = {
-    getTokenData() {
-      return { id: 1 };
-    },
-  };
+  let authService: jasmine.SpyObj<AuthService>;
+  let vagaService: jasmine.SpyObj<VagaService>;
+  let confirmationService: jasmine.SpyObj<ConfirmationService>;
+  let messageService: jasmine.SpyObj<MessageService>;
+  let routerMock: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', [
+      'getTokenData',
+    ]);
+    const vagaServiceSpy = jasmine.createSpyObj('VagaService', [
+      'atribuirVaga',
+      'apagarVaga',
+      'getVaga',
+    ]);
+
+    const confirmationServiceSpy = jasmine.createSpyObj('ConfirmationService', [
+      'confirm',
+    ]);
+
+    const messageServiceSpy = jasmine.createSpyObj('MessageService', ['add']);
+
+    const routerMockSpy = jasmine.createSpyObj('Router', ['navigate']);
+
     await TestBed.configureTestingModule({
       providers: [
-        { provide: VagaService },
+        { provide: VagaService, useValue: vagaServiceSpy },
+        { provide: AuthService, useValue: authServiceSpy },
         { provide: HttpClient },
         { provide: HttpHandler },
-        { provide: MessageService },
-        { provide: ConfirmationService },
-        { provide: Router, useValue: {} },
+        { provide: MessageService, useValue: messageServiceSpy },
+        { provide: ConfirmationService, useValue: confirmationServiceSpy },
+        { provide: Router, useValue: routerMockSpy },
         {
           provide: ActivatedRoute,
           useValue: {
             snapshot: {
               params: {
-                id: '1',
+                id: 1,
               },
             },
           },
         },
-        { provide: AuthService, useValue: mockAuthService },
       ],
       declarations: [VagaComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(VagaComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    vagaService = TestBed.inject(VagaService) as jasmine.SpyObj<VagaService>;
+    messageService = TestBed.inject(
+      MessageService
+    ) as jasmine.SpyObj<MessageService>;
+    confirmationService = TestBed.inject(
+      ConfirmationService
+    ) as jasmine.SpyObj<ConfirmationService>;
+    routerMock = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the "trabalhador" user ', () => {
+    const tokenData = {
+      id: 1,
+      nome: 'Nome',
+      Email: 'teste@teste.com',
+      Telefone: '123456789',
+      CPF: '12345678900',
+      CNPJ: '12345678000100',
+      Senha: '123',
+      Ativo: true,
+      isTrabalhador: true,
+    };
+    authService.getTokenData.and.returnValue(tokenData);
+    vagaService.getVaga.and.returnValue(of());
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(authService.getTokenData).toHaveBeenCalled();
+    expect(component.idUsuario).toEqual(1);
+    expect(component.isTrabalhador).toBeTrue();
+    expect(vagaService.getVaga).toHaveBeenCalledWith(1);
+    expect(component.route).toEqual('trabalhador/inicial');
+  });
+
+  it('should initialize the "empresa" user ', () => {
+    const tokenData = {
+      id: 1,
+      nome: 'Nome',
+      Email: 'teste@teste.com',
+      Telefone: '123456789',
+      CPF: '12345678900',
+      CNPJ: '12345678000100',
+      Senha: '123',
+      Ativo: true,
+      isTrabalhador: false,
+    };
+    authService.getTokenData.and.returnValue(tokenData);
+    vagaService.getVaga.and.returnValue(of());
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(authService.getTokenData).toHaveBeenCalled();
+    expect(component.idUsuario).toEqual(1);
+    expect(component.isTrabalhador).toBeFalse();
+    expect(vagaService.getVaga).toHaveBeenCalledWith(1);
+    expect(component.route).toEqual('empresa/inicial');
+  });
+
+  it('should registrarVaga', () => {
+    const tokenData = {
+      id: 1,
+      nome: 'Nome',
+      Email: 'teste@teste.com',
+      Telefone: '123456789',
+      CPF: '12345678900',
+      CNPJ: '12345678000100',
+      Senha: '123',
+      Ativo: true,
+      isTrabalhador: false,
+    };
+    authService.getTokenData.and.returnValue(tokenData);
+    vagaService.getVaga.and.returnValue(of());
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const data = {
+      idVaga: 1,
+      idTrabalhador: 1,
+      dataAceite: new Date(),
+    } as ITrabalhadorVaga;
+    vagaService.atribuirVaga.and.returnValue(of(null));
+
+    component.registrarVaga();
+
+    expect(vagaService.atribuirVaga).toHaveBeenCalledWith(data);
+    expect(messageService.add).toHaveBeenCalledWith({
+      severity: 'success',
+      summary: 'Sucesso',
+      detail: 'Vaga atribuída com sucesso',
+    });
+  });
+
+  it('should apagarVaga', () => {
+    const tokenData = {
+      id: 1,
+      nome: 'Nome',
+      Email: 'teste@teste.com',
+      Telefone: '123456789',
+      CPF: '12345678900',
+      CNPJ: '12345678000100',
+      Senha: '123',
+      Ativo: true,
+      isTrabalhador: false,
+    };
+    authService.getTokenData.and.returnValue(tokenData);
+    vagaService.getVaga.and.returnValue(of());
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    component.apagarVaga();
+    vagaService.apagarVaga.and.returnValue(of(null));
+
+    confirmationService.confirm.calls.first().args[0].accept();
+    routerMock.navigate.and.returnValue(Promise.resolve(true));
+
+    expect(vagaService.apagarVaga).toHaveBeenCalled();
+    expect(messageService.add).toHaveBeenCalledWith({
+      severity: 'success',
+      summary: 'Sucesso',
+      detail: 'Vaga apagada com sucesso',
+    });
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/empresa/inicial']);
+  });
+
+  it('should refresh', () => {
+    const tokenData = {
+      id: 1,
+      nome: 'Nome',
+      Email: 'teste@teste.com',
+      Telefone: '123456789',
+      CPF: '12345678900',
+      CNPJ: '12345678000100',
+      Senha: '123',
+      Ativo: true,
+      isTrabalhador: false,
+    };
+    authService.getTokenData.and.returnValue(tokenData);
+    vagaService.getVaga.and.returnValue(of());
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    component.refresh();
+
+    expect(component.mostrarModal).toBeTrue();
+    expect(vagaService.getVaga).toHaveBeenCalledWith(component.idVaga);
   });
 });
